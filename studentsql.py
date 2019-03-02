8b6e23519e7f68e69cde337bef9eb2d603cc3bf0
gmklamer <gmklamer999@gmail.com>
2019-03-02 16:14:05 -0500

    New sql file

---
+++ studentsql.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+conn = sqlite3.connect('mytest.db')
+cursor = conn.cursor()
+print "Let's input some students!"
+while True:
+	name = raw_input('Student\'s name: ')
+	username = raw_input(Student\'s username: ')
+	id_num = raw_input('Student\'s id: ')
+	sql = ''' insert into students
+		(name, username, id)
+		values
+			(:st_name, :st_username, :id_num)'''
+	cursor.execute(sql, {'st_name':name, 'st_username':username, 'id_num':id_num})
+	conn.commit()
+	cont = raw_input("Another student? ")
+	if cont[0].lower() == 'n':
+		break	
+cursor.close()
