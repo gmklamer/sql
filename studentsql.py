8948d566c799f4a7f54de05501eeaa2df6316b05
gmklamer <gmklamer999@gmail.com>
2019-03-03 13:19:34 -0500

    Added function to create database

--- studentsql.py
+++ studentsql.py
@@ -1,19 +1,32 @@
+# Python 3
 import sqlite3
 
-conn = sqlite3.connect('mytest.db')
+# Function to create a database
+def createdb:
+	fconn = sqlite3.connect('student.db')
+	cursor = fconn.cursor()
+	sql = '''create table students (
+		name text,
+		username text,
+		id int)'''
+	cursor.execute(sql)
+	cursor.close()
+	
+createdb()
+conn = sqlite3.connect('students.db')
 cursor = conn.cursor()
 print ("Let's input some students!")
 while True:
-	name = raw_input('Student\'s name: ')
-	username = raw_input('Student\'s username: ')
-	id_num = raw_input('Student\'s id: ')
+	name = input('Student\'s name: ')
+	username = input('Student\'s username: ')
+	id_num = input('Student\'s id: ')
 	sql = ''' insert into students
 		(name, username, id)
 		values
 			(:st_name, :st_username, :id_num)'''
 	cursor.execute(sql, {'st_name':name, 'st_username':username, 'id_num':id_num})
 	conn.commit()
-	cont = raw_input("Another student? ")
+	cont = input("Another student? ")
 	if cont[0].lower() == 'n':
 		break	
 cursor.close()
